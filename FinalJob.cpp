19acaac02c90f0c48c648aaaed79428be9022ed2
201711140033 <201711140033@LABCOMP2-PC25.facomp.ufpa.br>
2018-01-10 20:08:07 -0300

    Trabalho Final

---
+++ FinalJob.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+void print( vector< bool * > &, int );
+bool generateGene();
+void createPopulation( vector< bool * > &, int );
+double calcFitness( bool [ ] chrom, int, individual &, vector< product > &, double, double );
+
+struct product
+{
+	double weight;
+	double value;
+};
+
+struct individual
+{
+	double fitness;
+	vector< product > fenotipo;
+}
+
+void setallproduct( vector< bool * >  & );
+
+int main()
+{
+	int sizePopulation = 10;
+	int numGenes = 15;
+	int numGenerations = 100;
+	
+	vector< product > allproduct( 15 );
+	
+		
+	cout << "Programa para calcular o fitness de um gene!\n";
+	
+	while()
+	{
+		srand(( int ) time( 0 ));
+		
+		vector< bool * > population( sizePopulation );
+		
+		vector< individual > popDecod( sizePopulation );
+		
+	
+		createPopulation( population, numGenes );
+		print(population, numGenes);		
+		setallproduct( allproduct );
+		
+		for ( int i = 0; i < numGenerations; i++ )
+		{
+			for( n = 0; n < sizePopulation; n++ )
+				 popDecod[ n ].fitness = calcFitness( population[n], numGenes, popDecod[ n ], allproduct )
+		}
+		
+		
+	}
+	
+	
+	//função delete bool[]
+}
+
+void setallproduct( vector< product > &allproduct )
+{
+	allproduct[ 0 ].weight = 100;
+	allproduct[ 0 ].value = 1000;
+	
+	allproduct[ 1 ].weight = 1;
+	allproduct[ 1 ].value = 10000;
+	
+	allproduct[ 1 ].weight = 10;
+	allproduct[ 1 ].value = 100;
+	
+	allproduct[ 0 ].weight = 1000;
+	allproduct[ 0 ].value = 10;
+	
+	allproduct[ 1 ].weight = 100;
+	allproduct[ 1 ].value = 10;
+	
+	allproduct[ 1 ].weight = 10;
+	allproduct[ 1 ].value = 10000;
+	
+	allproduct[ 0 ].weight = 100;
+	allproduct[ 0 ].value = 1;
+	
+	allproduct[ 1 ].weight = 10;
+	allproduct[ 1 ].value = 10000;
+	
+	allproduct[ 0 ].weight = 1000;
+	allproduct[ 0 ].value = 10;
+	
+	allproduct[ 0 ].weight = 1;
+	allproduct[ 0 ].value = 1;
+	
+	allproduct[ 1 ].weight = 100;
+	allproduct[ 1 ].value = 100000;
+	
+	allproduct[ 1 ].weight = 1000;
+	allproduct[ 1 ].value = 100;
+	
+	allproduct[ 0 ].weight = 10000;
+	allproduct[ 0 ].value = 10000;
+	
+	allproduct[ 1 ].weight = 10;
+	allproduct[ 1 ].value = 10;
+	
+	allproduct[ 1 ].weight = 1;
+	allproduct[ 1 ].value = 1000;
+}
+
+bool generateGene()
+{
+	if( ( rand() % 100 + 1 ) <= 50 )
+		return false;
+		else return true;
+}
+void createPopulation( vector< bool * > &pop, int numGenes )
+{
+	for ( int i = 0; i < pop.size(); i++ )
+	{
+			pop[ i ] =  new bool[ numGenes ];
+			for ( int j = 0; j < numGenes; j++ )
+			     pop[ i ][ j ] = generateGene( );			
+		
+	}	
+			
+		
+}
+
+double calcFitness( bool [ ] chrom, int numGenes, individual &ind, vector< product > & allproduct, double Pmax, double Vmax )
+{
+	int fitness = 0;
+	
+	for( int i = 0; i < numGenes; i++ )
+		if( chrom[ i ] )
+		{
+			ind.fenotipo.push_back( allproduct[ i ] );
+			fitness += allproduct[ i ].value / Vmax + Pmax/ allproduct[ i ].weight;
+		}
+			
+			
+		
+}
+
+void print(vector< bool * > &pop, int numGenes)
+{
+	
+	for ( int i = 0; i < pop.size(); i++ )
+	{
+		for ( int j = 0; j < numGenes; j++ )
+			if( pop[ i ][ j ] )
+				cout << 1 << '\t';
+			else
+				cout << 0 << '\t';
+		cout << "\n";
+	}
+}
+
+
+
